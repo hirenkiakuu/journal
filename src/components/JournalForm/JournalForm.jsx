fe1391db7aa2fa8fe1505cfbd36285877d66de7c
Vladislav <vladdik9@gmail.com>
2024-06-21 15:56:10 +0500

    conspected second variation of dynamic stylization

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -70,10 +70,10 @@ const JournalForm = ({ onSubmit }) => {
 
   return (
       <form className='journal-form' onSubmit={addJournalNote}>
-        <input type="text" name='title' style={{ border: formValidState.title ? undefined : '1px solid red' }} />
-        <input type="date" name='date' style={{ border: formValidState.date ? undefined : '1px solid red' }} />
+        <input type="text" name='title' className={`input ${formValidState.title ? '' : 'invalid'}`} />
+        <input type="date" name='date' className={`input ${formValidState.date ? '' : 'invalid'}`} />
         <input type="text" name='tag'   />
-        <textarea name="text" id="" cols='30' rows='10' style={{ border: formValidState.text ? undefined : '1px solid red' }}></textarea>
+        <textarea name="text" id="" cols='30' rows='10' className={`input ${formValidState.text ? '' : 'invalid'}`}></textarea>
         <Button text='сохранить' />
       </form>
   );
